525ea79a56273f2f949ba1e866b0a48e11799ad7
Amir Tadrisi <amirtds@gmail.com>
2020-09-29 17:09:24 -0500

    Chore: change sex to gender

--- openedx/core/djangoapps/user_authn/views/registration_form.py
+++ openedx/core/djangoapps/user_authn/views/registration_form.py
@@ -603,7 +603,7 @@ class RegistrationFormFactory:
         """
         # Translators: This label appears above a dropdown menu on the registration
         # form used to select the user's gender.
-        gender_label = _(u"Sex")
+        gender_label = _(u"Gender")
 
         # The labels are marked for translation in UserProfile model definition.
         # pylint: disable=translation-of-non-string
